aa36114f86331ea58e7e83fe347f61402219fded
Alfonso Pruneda-Suarez <alfonso.pru@outlook.com>
2019-09-08 00:27:11 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -22,8 +22,8 @@ def shift_with_args(array)
   array.shift(2)  
 end
 
-def using_concat(arg1,arg2)
-  array.concat(arg2,arg1)
+def using_concat(array1,array2)
+  array.concat(array1,array2)
 end
 
 def using_insert(array, new_element)
