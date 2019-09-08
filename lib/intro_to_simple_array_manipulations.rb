150eaf7208a4b448a15b8422c8c30274bb389e6d
Alfonso Pruneda-Suarez <alfonso.pru@outlook.com>
2019-09-08 00:25:34 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -18,6 +18,10 @@ def using_shift(array)
   array.shift
 end
 
+def shift_with_args(array)
+  array.shift(2)  
+end
+
 def using_concat(arg1,arg2)
   array.concat(arg1,arg2)
 end
@@ -39,5 +43,5 @@ def using_delete(array,string)
 end
 
 def using_delete_at(array, item_to_delete)
-  array.delete_at(item_to_delete, array)
+  array.delete_at(array, item_to_delete)
 end
