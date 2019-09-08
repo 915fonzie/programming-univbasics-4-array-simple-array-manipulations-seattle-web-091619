68a0810a5a5080ef4ccfa37357e8ab1241d2d8ff
Alfonso Pruneda-Suarez <alfonso.pru@outlook.com>
2019-09-08 00:24:04 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -26,18 +26,18 @@ def using_insert(array, new_element)
   array.insert(4,new_element)
 end
 
-def using_uniq()
-  
+def using_uniq(array)
+  array.uniq
 end
 
-def using_flatten()
-  
+def using_flatten(array)
+  array.flatten
 end
 
-def using_delete()
-  
+def using_delete(array,string)
+  array.delete(string)
 end
 
-def using_delete_at()
-  
+def using_delete_at(array, item_to_delete)
+  array.delete_at(item_to_delete, array)
 end
